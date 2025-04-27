ee66edc0e2b951d8d72a399f9c1b17578f4e6535
vyin <limcolin0809@gmail.com>
2025-04-27 15:40:15 -0400

    add hero

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,15 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
-    public MyWorld() {
+public class MyWorld extends World 
+{
+    public MyWorld() 
+    {
         super(600, 400, 1);
+        
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
+        
+        Banana banana = new Banana();
+        addObject(banana, 100, 100);
     }
 }
